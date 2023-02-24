0437f228d3139a892b323034e1cbd483f3aad4af
Austin <pizzahi5@gmail.com>
2023-02-24 15:17:47 -0800

    Adding_openzeppelin

--- src/Profile.sol
+++ src/Profile.sol
@@ -2,23 +2,33 @@
 pragma solidity ^0.8.13;
 
 import "./interfaces/IProfile.sol";
+import "./mocks/Counters.sol";
 
 contract Profile is IProfile {
-    mapping(address => bool) authorizedContract;
+    using Counters for Counters.Counter;
 
-    mapping(address => mapping(uint256 => AttestParams)) attestations;
+    mapping(address => bool) authorizedContract;
+    
+    uint256 id;
+
+    //put in authorization contract
+    //mapping(address => mapping(uint256 => AttestParams)) attestations;
 
     function attest(AttestParams memory params) external {
-
+        Counters.increment(id);
     }
 
     //authorize = authorized contracts to do stuff
-    function authorize(address newAuthorizer) external {
-        
+    function addAuthorizer(address newAuthorizer) external {
+        authorizedContract[newAuthorizer] = true;
+    }
+
+    function removeAuthorizer(address badAuthorizer) external {
+        authorizedContract[badAuthorizer] = false;
     }
 
     //pointer to AttestParams
-    function revoke(AttestParams memory params) external {
+    function revoke(uint256 _id) external {
 
     }
 }
