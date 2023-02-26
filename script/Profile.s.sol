08a2f6aad0bb5b366fcbf3ddf242fdc8646bc6c9
Austin <pizzahi5@gmail.com>
2023-02-25 22:03:01 -0800

    Got_scrips_running

--- script/Profile.s.sol
+++ script/Profile.s.sol
@@ -5,12 +5,12 @@ import "forge-std/Script.sol";
 import "../src/Profile.sol";
 import "../src/EmployerDAO.sol";
 
-contract TestState is Script {
+contract ProfileScript is Script {
     function setUp() public {}
 
     function run() public {
-        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        // vm.startBroadcast(deployerPrivateKey);
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         // deploy some contracts for funsies.
         Profile profile = new Profile();
